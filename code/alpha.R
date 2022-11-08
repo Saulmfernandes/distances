0095ac96c0c66368d840d9d00fa7ae34fbeeb2e6
Saulmfernandes <117680461+Saulmfernandes@users.noreply.github.com>
2022-11-08 21:03:27 +0100

    Update alpha.R

--- code/alpha.R
+++ code/alpha.R
@@ -23,19 +23,21 @@ shared <- read_tsv("data/mice.shared") %>%
 rand <- shared %>%
   uncount(value) %>%
   mutate(name = sample(name)) %>%
-  count(Group, name, name="value")
+  count(Group, name, name="value") #takes all of the data from shared but keep taxa separated (225 random samples)
 
+
+##Writing the function from screcht, without a package###
 richness <- function(x){
   
-  # r <- sum(x > 0)
+  # r <- sum(x > 0) # this will return the number of values of x that are greater than zero
   # return(r)
   
-  sum(x>0)
+  sum(x>0) #simplified version of the code above
 }
 
 shannon <- function(x){
   
-  rabund <- x[x>0]/sum(x)
+  rabund <- x[x>0]/sum(x) # limited to x>0 because if there is a zero, doesn't work
   -sum(rabund * log(rabund))
   
 }
@@ -50,7 +52,7 @@ simpson <- function(x){
 
 rand %>%
   group_by(Group) %>%
-  summarize(sobs = richness(value),
+  summarize(sobs = richness(value), # summarized by richness
             shannon = shannon(value),
             simpson = simpson(value),
             invsimpson = 1/simpson,
@@ -65,9 +67,9 @@ rand %>%
 
 rand %>%
   group_by(Group) %>%
-  summarize(sobs = specnumber(value),
-            shannon = diversity(value, index="shannon"),
-            simpson = diversity(value, index="simpson"),
+  summarize(sobs = specnumber(value), #richness
+            shannon = diversity(value, index="shannon"), #shannon value
+            simpson = diversity(value, index="simpson"), #simpson, not the bias corrected version
             invsimpson = 1/simpson,
             n = sum(value)) %>%
   pivot_longer(cols=c(sobs, shannon, invsimpson, simpson),
